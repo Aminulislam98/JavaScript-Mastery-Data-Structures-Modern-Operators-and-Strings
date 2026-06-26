37020610383b7d7555f11b102a102d8a97fdee94
Aminul islam <aminurrohman98@gmail.com>
2026-06-26 22:02:07 +0100

    learned about intersection method that is very recently added in javaScript

--- script.js
+++ script.js
@@ -927,10 +927,34 @@ newSet.delete('Mango');
 //   console.log(item);
 // }
 
-console.log([...newSet]);
-const staff = ['manager', 'chef', 'waiter', 'chef', 'manager'];
-console.log(staff);
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+// console.log([...newSet]);
+// const staff = ['manager', 'chef', 'waiter', 'chef', 'manager'];
+// console.log(staff);
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
 
-console.log(new Set('aminul'));
+// console.log(new Set('aminul'));
+
+// New operation to make set useful
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Intersection', commonFoods);
+console.log([...commonFoods]);
