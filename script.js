407f0dfa4d07156da17515cbdfba52981b7a710c
Aminul islam <aminurrohman98@gmail.com>
2026-06-26 19:47:55 +0100

    solved exercise num 4

--- script.js
+++ script.js
@@ -885,3 +885,31 @@ for (const [team, num] of odds) {
   totalScore += num;
 }
 console.log(`Average odd is: ${totalScore / odds.length}`);
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw").
+
+// for (const [team, num] of odds) {
+//   team;
+// }
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+// {
+//   Gnarby: 1,
+//   Hummels: 1,
+//   Lewandowski: 2
+// }
+
+// const scorers = {};
+
+// for (const player of game.scored) {
+//   if (scorers[player]) {
+//     scorers[player]++;
+//   } else {
+//     scorers[player] = 1;
+//   }
+// }
+// console.table(scorers);
