20cf681fe6493e2ce2e2686cb5c5037b4489465f
Aminul islam <aminurrohman98@gmail.com>
2026-06-28 20:17:17 +0100

    solve the problem , challenge

--- script.js
+++ script.js
@@ -1209,18 +1209,56 @@ const uniqueEvents = [...events];
 
 // alertMessage(3);
 
-const messyName = 'first_name';
-function capitalizedName(name) {
-  const lowerCaseAndSplit = name.replaceAll('_', ' ').toLowerCase().split(' ');
-  const properName = [];
-  for (const n of lowerCaseAndSplit) {
-    if (n === '') {
-      continue;
-    } else {
-      properName.push(n[0].toUpperCase() + n.slice(1));
-    }
-  }
-  return properName.join('');
-}
+// const messyName = 'first_name';
+// function capitalizedName(name) {
+//   const lowerCaseAndSplit = name.replaceAll('_', ' ').toLowerCase().split(' ');
+//   const properName = [];
+//   for (const n of lowerCaseAndSplit) {
+//     if (n === '') {
+//       continue;
+//     } else {
+//       properName.push(n[0].toUpperCase() + n.slice(1));
+//     }
+//   }
+//   return properName.join('');
+// }
 
-console.log(capitalizedName('aminUl_isLaM'));
+// console.log(capitalizedName('aminUl_isLaM'));
+
+// const submitBtn = document.querySelector('.submit');
+
+// submitBtn.addEventListener('click', function () {
+//   const text = document.querySelector('.textAria').value;
+//   const rows = text.split('\n');
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
+
+/* underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure */
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  // console.log(flight);
+  const [type, from, to, time] = flight.replaceAll('_', ' ').split(';');
+  const output =
+    `${type.trim().toLowerCase().startsWith('delayed') ? '🔴' : '✅'}${type} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(
+      50,
+    );
+  console.log(output);
+}
